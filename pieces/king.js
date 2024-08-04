e00f6ffda61ade42bd1f2dd921a336812ecf5292
Akhilendre singh rawat <akhilendre.rawat@daffodilsw.com>
2024-08-04 12:35:28 +0530

    fix: white king and queens initial positions

--- pieces/king.js
+++ pieces/king.js
@@ -2,7 +2,7 @@ import { Piece } from "./piece.js";
 
 export function initializeKings() {
     const blackKing = new Piece(0, 4, "b", 'k');
-    const whiteKing = new Piece(7, 3, "w", 'k');
+    const whiteKing = new Piece(7, 4, "w", 'k');
     this.blackPieces.alive['0-4'] = blackKing;
     this.whitePieces.alive['7-3'] = whiteKing;
 };
--- pieces/queen.js
+++ pieces/queen.js
@@ -2,7 +2,7 @@ import { Piece } from "./piece.js";
 
 export function initializeQueens() {
     const blackQueen = new Piece(0, 3, "b", 'q');
-    const whiteQueen = new Piece(7, 4, "w", 'q');
+    const whiteQueen = new Piece(7, 3, "w", 'q');
     this.blackPieces.alive['0-3'] = blackQueen;
     this.whitePieces.alive['7-4'] = whiteQueen;
 };
